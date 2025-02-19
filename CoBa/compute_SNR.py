2b304c5d2d7a40a4bc4c33fb6c9a77963b208548
Thibeau Wouters <thibeauwouters@gmail.com>
2025-02-19 02:54:07 -0800

    Script to loop over CoBa and compute SNR seems OK now

--- CoBa/compute_SNR.py
+++ CoBa/compute_SNR.py
@@ -1,6 +1,5 @@
 import numpy as np 
 import tqdm
-import matplotlib.pyplot as plt
 import bilby
 import utils
 import json
@@ -8,15 +7,25 @@ import json
 # Make bilby silent, otherwise will print for each injected signal
 bilby.core.utils.logger.setLevel("ERROR")
 
-for pop_str in ["BBH"]: # ["BBH", "BNS"]
+for pop_str in ["BBH", "BNS"]:
     
-    # Will store the SNR in this dict for the entire population, then add it to the catalogue at the end
-    all_snr_dict = {"ET1": [], "ET2": [], "ET3": [], "ET": [], "CE": []}
-    is_tidal = pop_str == "BNS"
+    print(f"Now looping over the {pop_str} population...")
+    
+    # Get the correct number of events to loop over
     if pop_str == "BBH":
         N = utils.N_BBH_COBA
     else:
         N = utils.N_BNS_COBA
+    
+    # Toggle tidal effects to be added if we are looking at BNS
+    is_tidal = pop_str == "BNS"
+    
+    # We will store the SNR in this dict for the entire population, then add it to the catalogue at the end
+    all_snr_dict = {"ET1": np.zeros(N), 
+                    "ET2": np.zeros(N), 
+                    "ET3": np.zeros(N), 
+                    "ET": np.zeros(N), 
+                    "CE": np.zeros(N)}
     
     for idx in tqdm.tqdm(range(N)):
         event = utils.get_CoBa_event("BNS", idx)
@@ -24,18 +33,19 @@ for pop_str in ["BBH"]: # ["BBH", "BNS"]
         snr_dict = utils.inject_and_get_SNR(event)
         
         # Add the SNR to the complete list
-        all_snr_dict["ET1"].append(snr_dict["ET1"])
-        all_snr_dict["ET2"].append(snr_dict["ET2"])
-        all_snr_dict["ET3"].append(snr_dict["ET3"])
-        all_snr_dict["ET"].append(snr_dict["ET"])
-        all_snr_dict["CE"].append(snr_dict["CE"])
+        for key in list(all_snr_dict.keys()):
+            all_snr_dict[key][idx] = snr_dict[key]
         
-    # Add the SNR to the CoBa catalogue
-    utils.CoBa_events_dict[pop_str]["ET1_SNR"] = all_snr_dict["ET1"]
-    utils.CoBa_events_dict[pop_str]["ET2_SNR"] = all_snr_dict["ET2"]
-    utils.CoBa_events_dict[pop_str]["ET3_SNR"] = all_snr_dict["ET3"]
-    utils.CoBa_events_dict[pop_str]["CE"] = all_snr_dict["CE"]
-    
+    # Now add all the SNRs to the CoBa catalogue dict
+    for key in list(all_snr_dict.keys()):
+        utils.CoBa_events_dict[pop_str][f"{key}_SNR"] = all_snr_dict[key]
+        
+    # NOTE: Numpy arrays are not compatible with JSON, so call tolist() before saving
+    for key in list(utils.CoBa_events_dict[pop_str].keys()):
+        utils.CoBa_events_dict[pop_str][key] = utils.CoBa_events_dict[pop_str][key].tolist()
+        
     # Save it as JSON:
-    with open(f"CoBa_events_{pop_str}.json", "w") as f:
-        json.dump(utils.CoBa_events_dict, f, indent = 4)
+    filename = f"CoBa_events_{pop_str}.json"
+    print(f"Saving the updated CoBa catalogue, with the SNRs, to {filename}")
+    with open(filename, "w") as f:
+        json.dump(utils.CoBa_events_dict[pop_str], f, indent = 4)
